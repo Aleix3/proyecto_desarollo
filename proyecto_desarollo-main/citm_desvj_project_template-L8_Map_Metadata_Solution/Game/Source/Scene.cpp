ddff05a1ad85ec5a6df95f3b73ac563cfd857b7e
Aleix3 <Aleixbotella.1@gmail.com>
2023-10-30 14:20:49 +0100

    mapa actu

--- proyecto_desarollo-main/citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/Scene.cpp
+++ proyecto_desarollo-main/citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/Scene.cpp
@@ -93,7 +93,7 @@ bool Scene::Update(float dt)
 	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 		app->render->camera.x += (int)ceil(camSpeed * dt);
 
-	app->render->camera.x = 2*(-player->position.x) + 600 ;
+	/*app->render->camera.x = 2*(-player->position.x) + 600 ;*/
 	// Renders the image in the center of the screen 
 	//app->render->DrawTexture(img, (int)textPosX, (int)textPosY);
 
